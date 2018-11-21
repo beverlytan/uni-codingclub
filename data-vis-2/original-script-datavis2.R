53e0ad9550f97b390ec91d1e9635309ec6bcfad4
beverlytan <s1550222@sms.ed.ac.uk>
2018-11-21 23:25:47 +0000

    Beverly edited datavis2 modified script

--- data-vis-2/original-script-datavis2.R
+++ data-vis-2/original-script-datavis2.R
@@ -128,5 +128,129 @@ species_counts <- magic_veg %>%
 
 ggsave("data-vis-2/histogram.png", width = 7, height = 5, dpi = 300)
 
+# Creating boxplots ---- 
 
-getwd()
+yearly_counts <- magic_veg %>%
+  group_by(land, plot, year) %>%  
+  summarise(Species_number = length(unique(species))) %>%
+  ungroup() %>%
+  mutate(plot = as.factor(plot))
+
+View(yearly_counts)
+
+(boxplot <- ggplot(yearly_counts, aes(plot, Species_number, fill = land)) +
+  geom_boxplot())
+
+(boxplot <- ggplot(yearly_counts, aes(x = plot, y = Species_number, fill = land)) +
+  geom_boxplot() +
+  scale_x_discrete(breaks = 1:6) +
+  scale_fill_manual(values = c("rosybrown1", "#deebf7"),
+                    breaks = c("Hogsmeade","Narnia"),
+                    name="Land of magic",
+                    labels=c("Hogsmeade", "Narnia")) +
+  labs(title = "Species richness by plot", 
+       x = "\n Plot number", y = "Number of species \n") + 
+  theme_bw() + 
+  theme() + 
+  theme(panel.grid = element_blank(), 
+        axis.text = element_text(size = 12), 
+        axis.title = element_text(size = 12), 
+        plot.title = element_text(size = 14, hjust = 0.5, face = "bold"), 
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), units = , "cm"), 
+        legend.position = "bottom", 
+        legend.box.background = element_rect(color = "grey", size = 0.3)))
+  
+ggsave("data-vis-2/magical-sp-rich-boxplot1.png", width = 7, height = 5, dpi = 300)
+
+# If I wanted Narnia to come before Hogsmeade
+
+yearly_counts$land <- factor(yearly_counts$land, 
+                             levels = c("Narnia", "Hogsmeade"),
+                             labels = c("Narnia", "Hogsmeade"))
+
+(boxplot <- ggplot(yearly_counts, aes(x = plot, y = Species_number, fill = land)) +
+    geom_boxplot() +
+    scale_x_discrete(breaks = 1:6) +
+    scale_fill_manual(values = c("#deebf7", "rosybrown1"),
+                      breaks = c("Narnia","Hogsmeade"),
+                      name = "Land of magic",
+                      labels = c("Narnia", "Hogsmeade")) +
+    labs(title = "Species richness by plot", 
+         x = "\n Plot number", y = "Number of species \n") + 
+    theme_bw() + 
+    theme() + 
+    theme(panel.grid = element_blank(), 
+          axis.text = element_text(size = 12), 
+          axis.title = element_text(size = 12), 
+          plot.title = element_text(size = 14, hjust = 0.5, face = "bold"), 
+          plot.margin = unit(c(0.5,0.5,0.5,0.5), units = , "cm"), 
+          legend.position = "bottom", 
+          legend.box.background = element_rect(color = "grey", size = 0.3)))
+
+# If I wanted to reorder plot number to be 6 first, then 1234, 
+
+yearly_counts$plot <- factor(yearly_counts$plot, 
+                             levels = c("6", "1", "2", "3", "4", "5"),
+                             labels = c("6", "1", "2", "3", "4", "5"))
+
+(boxplot2 <- ggplot(yearly_counts, aes(x = plot, y = Species_number, fill = land)) +
+    geom_boxplot() +
+    scale_x_discrete(breaks = 1:6) +
+    scale_fill_manual(values = c("#deebf7", "rosybrown1"),
+                      breaks = c("Narnia","Hogsmeade"),
+                      name = "Land of magic",
+                      labels = c("Narnia", "Hogsmeade")) +
+    labs(title = "Species richness by plot", 
+         x = "\n Plot number", y = "Number of species \n") + 
+    theme_bw() + 
+    theme() + 
+    theme(panel.grid = element_blank(), 
+          axis.text = element_text(size = 12), 
+          axis.title = element_text(size = 12), 
+          plot.title = element_text(size = 14, hjust = 0.5, face = "bold"), 
+          plot.margin = unit(c(0.5,0.5,0.5,0.5), units = , "cm"), 
+          legend.position = "bottom", 
+          legend.box.background = element_rect(color = "grey", size = 0.3)))
+
+
+# Mixed effect model results ----
+
+heights <- magic_veg %>%
+  filter(!is.na(height)) %>%  
+  group_by(year, land, plot, id) %>%
+  summarise(Max_Height = max(height)) %>%  # Calculating max height
+  ungroup() %>%  # Need to ungroup so that the pipe doesn't get confused
+  group_by(year, land, plot) %>%
+  summarise(Height = mean(Max_Height))  # Calculating mean max height
+
+ggplot(heights, aes(year, Height, colour = land)) +
+  geom_point() +
+  theme_bw()
+
+ggplot(heights, aes(year, Height, colour = land)) +
+  geom_point() +
+  theme_bw() +
+  stat_smooth(method = "lm")
+
+library(nlme)
+
+# Using the square brackets to subset the data just for Hogsmeade
+lm_heights<-lme(Height ~ year, random = ~1|year/plot, data = heights[heights$land == "Hogsmeade",])
+summary(lm_heights)
+
+# Using the square brackets to subset the data just for Narnia
+lm_heights2<-lme(Height ~ year, random = ~1|year/plot, data = heights[heights$land == "Narnia",])
+summary(lm_heights2)
+
+theme_coding <- function(){
+  theme_bw()+
+    theme(axis.text.x = element_text(size = 12, angle = 45, vjust = 1, hjust = 1),
+          axis.text.y = element_text(size = 12),
+          axis.title = element_text(size = 14),
+          panel.grid = element_blank(),
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = , "cm"),
+          plot.title = element_text(size = 20, vjust = 1, hjust = 0.5),
+          legend.text = element_text(size = 12, face = "italic"),
+          legend.title = element_blank(),
+          legend.position=c(0.9, 0.9))
+}
